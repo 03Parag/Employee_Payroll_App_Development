0196601a17fa933ddd1db074ca0e9d1c0001f002
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-25 10:25:24 +0530

    [Parag Agrawal] Added problems ability for services layer to store the employee payroll app

---
+++ com/example/Employee_Payroll_App_Development/Section2_UC3_Store_Services_Layer/Model/EmployeeModel_Store.java
@@ -0,0 +1,48 @@
+package com.example.Employee_Payroll_App_Development.Section2_UC3_Store_Services_Layer.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class EmployeeModel_Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private double salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
+
