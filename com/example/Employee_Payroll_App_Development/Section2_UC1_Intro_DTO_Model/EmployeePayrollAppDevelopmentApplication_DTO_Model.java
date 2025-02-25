8b3a85978128b21cdcda05fb73b4dc11938139a0
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-25 10:06:37 +0530

    [Parag Agrawal] Added problem to introduce DTO and model to employee payroll app

---
+++ com/example/Employee_Payroll_App_Development/Section2_UC1_Intro_DTO_Model/EmployeePayrollAppDevelopmentApplication_DTO_Model.java
@@ -0,0 +1,11 @@
+package com.example.Employee_Payroll_App_Development.Section2_UC1_Intro_DTO_Model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppDevelopmentApplication_DTO_Model {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppDevelopmentApplication_DTO_Model.class, args);
+    }
+}
