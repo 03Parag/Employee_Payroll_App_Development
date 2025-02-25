86444c4bd243eefd4e3cf666b3d04205721675f8
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-25 09:59:21 +0530

    [Parag Agrawal] Added problem to create a Rest controller to demonstrate the various HTTP methods

---
+++ com/example/Employee_Payroll_App_Development/UC2_Demonstrate_Various_Methods/EmployeePayrollAppDevelopmentApplication_Methods.java
@@ -0,0 +1,11 @@
+package com.example.Employee_Payroll_App_Development.UC2_Demonstrate_Various_Methods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppDevelopmentApplication_Methods {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppDevelopmentApplication_Methods.class, args);
+    }
+}
