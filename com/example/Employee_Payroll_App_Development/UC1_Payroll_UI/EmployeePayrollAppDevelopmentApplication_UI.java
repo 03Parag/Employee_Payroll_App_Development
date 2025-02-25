117f83b168bcfc8f7b483b55e5e7ba9e47131416
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-25 09:53:01 +0530

    [Parag Agrawal] Added problem to create a employee payroll spring project to cater to REST request from employee payroll UI

---
+++ com/example/Employee_Payroll_App_Development/UC1_Payroll_UI/EmployeePayrollAppDevelopmentApplication_UI.java
@@ -0,0 +1,13 @@
+package com.example.Employee_Payroll_App_Development.UC1_Payroll_UI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppDevelopmentApplication_UI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppDevelopmentApplication_UI.class, args);
+	}
+
+}
