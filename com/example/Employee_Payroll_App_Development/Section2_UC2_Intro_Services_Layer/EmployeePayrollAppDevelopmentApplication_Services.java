167d01ace0b6deb45b83c208e62dbd2598f6ff4f
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-25 10:14:41 +0530

    [Parag Agrawal] Added problem to introduce services layer in employee payroll app

---
+++ com/example/Employee_Payroll_App_Development/Section2_UC2_Intro_Services_Layer/EmployeePayrollAppDevelopmentApplication_Services.java
@@ -0,0 +1,12 @@
+package com.example.Employee_Payroll_App_Development.Section2_UC2_Intro_Services_Layer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppDevelopmentApplication_Services {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppDevelopmentApplication_Services.class, args);
+    }
+}
+
