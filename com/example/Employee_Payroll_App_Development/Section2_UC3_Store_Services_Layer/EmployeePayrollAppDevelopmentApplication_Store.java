854d50cfa5ddb272cfde9e5830a85476f019058a
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-25 10:23:04 +0530

    [Parag Agrawal] Added problems ability for services layer to store the employee payroll app

---
+++ com/example/Employee_Payroll_App_Development/Section2_UC3_Store_Services_Layer/EmployeePayrollAppDevelopmentApplication_Store.java
@@ -0,0 +1,12 @@
+package com.example.Employee_Payroll_App_Development.Section2_UC3_Store_Services_Layer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppDevelopmentApplication_Store {
+    public static void main(String[] args) {
+        SpringApplication.run(com.example.Employee_Payroll_App_Development.Section2_UC3_Store_Services_Layer.EmployeePayrollAppDevelopmentApplication_Store.class, args);
+    }
+}
+
